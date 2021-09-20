3f223807e0edf8c48cfb9b11c090f38d36f3ab39
nookesh <nookeshkarri7@gmail.com>
2021-09-20 17:39:05 +0530

    Added Url Validation Check

--- bookmarkmaker/bookmarkmakerapp/forms.py
+++ bookmarkmaker/bookmarkmakerapp/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from bookmarkmakerapp.models import bookmarkMakertable
+
 class bookmarkMakerForm(forms.ModelForm):
+    site_url=forms.CharField()
     class Meta:
         model=bookmarkMakertable
         fields="__all__"
+
+    def clean(self):
+        cleaned_data=super().clean()
+        url=cleaned_data['site_url']
+        if url[-4:]!=".com":
+            raise forms.ValidationError("Please Enter Valid URL")
+        return url
